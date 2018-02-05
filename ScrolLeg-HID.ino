05b31e915d093cc026cd3450227cb79f0fc323eb
Amit Bourmad <amitburmad@gmail.com>
2018-02-05 15:55:14 +0200

    Readme

--- ScrolLeg-HID.ino
+++ ScrolLeg-HID.ino
@@ -1,13 +1,35 @@
+/**
+ * ScrolLeg - Arduino controlled bluetooth device, that lets you scroll with your legs
+ * 
+ * Device contains 2 buttons and WT12 bluetooth chip
+ * Each buttons sends a different command to the blueooth chip,
+ * One for scroll up, one for down.
+ * 
+ * 
+ */
+
 #include <avr/pgmspace.h>
 
 
-// WT12 Settings
-//------------------------------------------------------------------------------
-
-const char wt12_init_0[] PROGMEM = "SET BT NAME Amit";
+/**
+ * WT12 Setting
+ * Will run on setup()
+ */
+const char wt12_init_0[] PROGMEM = "SET BT NAME ScrolLeg";
 const char wt12_init_1[] PROGMEM = "SET BT PAIR *";
 
-#define WT12_RESET  7
+/**
+ * Up/Down button pin numbers
+ */
+#define UP_BUTTON     2
+#define DOWN_BUTTON   4
+/**
+ * WT12 Reset pin connection
+ */
+#define WT12_RESET    7
+/**
+ * Number of flash commands to run on chip at startup
+ */
 #define NUM_INIT_MSG  2
 
 // http://forum.arduino.cc/index.php?topic=272313.0
@@ -19,21 +41,8 @@ PGM_P const string_table[]  PROGMEM {
 
 char buffer[60]; // used for printing these init messages
 
-
-// Set buttons pin numbers
-const int UP_BUTTON = 2;
-const int DOWN_BUTTON = 4;
-
 // The Mouse HID command prefix, as hex array
 const byte packetPrefix[] = { 0x9F, 0x05, 0xA1, 0x01 }; 
-
-// Check if it works
-uint8_t bluetoothPacket[7] = {0x9F, 0x05, 0xA1, 0x01};
-//
-//bluetoothPacket[0] = 0x9F;
-//bluetoothPacket[1] = 0x05;
-//bluetoothPacket[2] = 0xA1;
-//bluetoothPacket[3] = 0x01;
 
 
 // Up/Down commands translated to hex array
@@ -46,26 +55,18 @@ byte MOUSE_DOWN[] = { 0xF0, 0x00, 0x00 };
  */
 void sendMousePacket(byte * packetExtra) {  
   // TODO: Check why loop doesn't work here
-  
+
+  // Send the mouse HID packet prefix
   Serial.write((byte)packetPrefix[0]);
   Serial.write((byte)packetPrefix[1]);
   Serial.write((byte)packetPrefix[2]);
   Serial.write((byte)packetPrefix[3]);
 
+  // Sends the actual mouse action
   Serial.write((byte)packetExtra[0]);
   Serial.write((byte)packetExtra[1]);
   Serial.write((byte)packetExtra[2]);
-  
-  // Send the command prefix
-//  for (byte i = 0; i < sizeof(packetPrefix); i++) {
-//    Serial.write((byte)packetPrefix[i]);
-//  }
-  
-//  // Send the actual wanted command packet
-//  for (byte i = 0; i < sizeof(packetExtra); i++) {
-//    Serial.write((byte)packetExtra[i]);
-//  }
-}
+ }
 
 /**
  * Scroll up command wrapper
@@ -104,32 +105,32 @@ void buttonActivity() {
     }
 }
 
-void initBluetooth()
-{
-    // Reset the WT12 module
+/**
+ * Starting bluetooth chip actions
+ */
+void initBluetooth() {
+    // Reset the WT12 module on startup, in case something gets stuck
     pinMode(WT12_RESET, OUTPUT);
     digitalWrite(WT12_RESET, HIGH);
     delay(10);
     digitalWrite(WT12_RESET, LOW);
 
-    Serial.println("\n\n");
-    Serial.println("AT");
+    log("\n\n");
     delay(1000);
 
-    // do the WT12 init
-    for (byte i = 0; i < NUM_INIT_MSG; i++)
-    {
+    // Runs the startup commands that we defined above, on the bluetooth chip
+    for (byte i = 0; i < NUM_INIT_MSG; i++) {
         strcpy_P(buffer, (char*)pgm_read_word(&(string_table[i])));
         Serial.println(buffer);
         delay(100);
     }
+    
     delay(1000);
-    Serial.flush(); // it echoes back everything we say?
-    Serial.println("RESET");
+    Serial.flush();
+    log("RESET");
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
     
   pinMode(UP_BUTTON, INPUT_PULLUP);
@@ -140,7 +141,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   buttonActivity();
 
   delay(200);
